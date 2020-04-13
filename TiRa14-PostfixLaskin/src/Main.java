bc4b52c027dc27b735db0936b56aea2d7984f635
wesenbergg <b.b.j.1998@gmail.com>
2020-04-13 15:55:36 +0300

    m

---
+++ TiRa14-PostfixLaskin/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Tehtävään on otettu mallia sivulta: https://mrtan.me/post/19.html
+ * @author Boriss
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner lukija = new Scanner(System.in);
+    	PrefixLaskin laskin = new PrefixLaskin();
+    	String syote;
+        String[] strArr;
+
+        while(true) {
+        	//Esimerkkisyöte "2 5 3 + *" tulostaa "16.0"
+        	System.out.println("Anna syöte. Erottele luvut ja merkit välilyönnein.");
+        	syote = lukija.nextLine();
+        	if(syote.isEmpty()) break;
+        	strArr = syote.split(" ");
+        	System.out.println(laskin.calculate(strArr));
+        }
+    }
+
+    
+}
---
+++ TiRa14-PostfixLaskin/src/PrefixLaskin.java
@@ -0,0 +1,57 @@
+import java.util.Stack;
+
+public class PrefixLaskin {
+	
+	public double calculate(String[] strArr) {
+        Stack<Double> pino = new Stack<Double>();
+        pino.add(0.0); //Handle empty stack exceptionin.
+
+        for(String str : strArr) {
+            if (str.trim().isEmpty()) continue;
+            
+            switch (str) {
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                	double vasen = pino.pop(); //Ottaa pinon kaksi päällimmäistä lukua
+                    double oikea = pino.pop();
+  
+                    double tulos = 0;
+                    switch(str) {
+                        case "+":
+                            tulos = vasen + oikea;
+                            break;
+                        case "-":
+                            tulos = vasen - oikea;
+                            break;
+                        case "*":
+                            tulos = vasen * oikea;
+                            break;
+                        case "/":
+                            try {
+                            	if(oikea == 0) throw new ArithmeticException();
+                            } catch (ArithmeticException e) {
+                            	System.out.println("Nollalla jako ei onnistu");
+                            	return 0;
+                            }
+                            tulos = vasen / oikea;
+                            break;
+                        default:
+                            break;
+                    }
+                    pino.push(tulos);
+                    break;
+                default:
+                    try {
+                    	double luku = Double.parseDouble(str);
+                    	pino.push(luku);
+                    }catch(Exception e) {System.out.println(str + " is not a number."); }
+                    break;  
+            }
+        }
+        
+        return pino.pop();	//Palautetaan pinon päällimmäinen luku esimerkiksi syötteellä "8 10 2 3 5 4 + *" pinon lopputulos on "8 10 2 27".
+        					//Jämä luvut jää lillumaan eteen.
+    }
+}
