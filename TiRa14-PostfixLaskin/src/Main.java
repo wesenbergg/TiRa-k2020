f820a55d99bb365626b1448c5bc3d689a64e17a6
wesenbergg <b.b.j.1998@gmail.com>
2020-11-20 09:52:51 +0200

    -m

--- TiRa14-PostfixLaskin/src/Main.java
+++ TiRa14-PostfixLaskin/src/Main.java
@@ -1,30 +1,33 @@
 import java.util.Scanner;
-import java.util.Stack;
 
 /**
- * Tehtävään on otettu mallia sivulta: https://mrtan.me/post/19.html
+ * Tehtävään on otettu mallia sivulta: https://mrtan.me/post/19.html .
  * @author Boriss
  *
  */
 
 public class Main {
-    public static void main(String[] args) {
+	/**
+	 * Main.
+	 * @param args default
+	 */
+    public static void main(final String[] args) {
     	Scanner lukija = new Scanner(System.in);
     	PostfixLaskin laskin = new PostfixLaskin();
     	String syote;
         String[] strArr;
 
-        while(true) {
+        while (true) {
         	//Esimerkkisyöte "2 5 3 + *" tulostaa "16.0"
-        	System.out.println("Anna syöte. Erottele luvut ja merkit välilyönnein.");
+        	System.out.println("Anna syöte."
+        	+ "Erottele luvut ja merkit välilyönnein.");
         	syote = lukija.nextLine();
-        	if(syote.isEmpty()) break;
+        	if (syote.isEmpty()) {
+        		break;
+        	}
         	strArr = syote.split(" ");
         	System.out.println(laskin.calculate(strArr));
         }
-        
         lukija.close();
     }
-
-    
 }
--- TiRa14-PostfixLaskin/src/PostfixLaskin.java
+++ TiRa14-PostfixLaskin/src/PostfixLaskin.java
@@ -1,24 +1,30 @@
 import java.util.Stack;
 
 public class PostfixLaskin {
-	
-	public double calculate(String[] strArr) {
+	/**
+	*
+	* @param strArr
+	* @return double
+	*/
+	public double calculate(final String[] strArr) {
         Stack<Double> pino = new Stack<Double>();
         pino.add(0.0); //Handle empty stack exceptionin.
 
-        for(String str : strArr) {
-            if (str.trim().isEmpty()) continue;
-            
+        for (String str : strArr) {
+            if (str.trim().isEmpty()) {
+            	continue;
+            }
+
             switch (str) {
                 case "+":
                 case "-":
                 case "*":
                 case "/":
-                	double vasen = pino.pop(); //Ottaa pinon kaksi päällimmäistä lukua
+                	//Ottaa pinon kaksi päällimmäistä lukua
+                	double vasen = pino.pop();
                     double oikea = pino.pop();
-  
                     double tulos = 0;
-                    switch(str) {
+                    switch (str) {
                         case "+":
                         	tulos = vasen + oikea;
                             break;
@@ -30,7 +36,9 @@ public class PostfixLaskin {
                             break;
                         case "/":
                             try {
-                            	if(oikea == 0) throw new ArithmeticException();
+                            	if (oikea == 0) {
+                            		throw new ArithmeticException();
+                            	}
                             } catch (ArithmeticException e) {
                             	System.out.println("Nollalla jako ei onnistu");
                             	return 0;
@@ -40,19 +48,26 @@ public class PostfixLaskin {
                         default:
                             break;
                     }
-                    System.out.println("Operaattori '" + str + "'\nPinosta poistetut: " + vasen + " ja " + oikea + ". Tilalle lisätty " + tulos); //Pinon päivitys tulostus
+                    //Pinon päivitys tulostus
+                    System.out.println("Operaattori '" + str
+                    		+ "'\nPinosta poistetut: " + vasen + " ja "
+                    		+ oikea + ". Tilalle lisätty " + tulos);
                     pino.push(tulos);
                     break;
                 default:
                     try {
                     	pino.push(Double.parseDouble(str));
-                    	System.out.println("Pinoon lisätty luku: " + str); //Pinon päivitys tulostus
-                    }catch(Exception e) {System.out.println(str + " is not a number."); }
-                    break;  
+                    	//Pinon päivitys tulostus
+                    	System.out.println("Pinoon lisätty luku: " + str);
+                    } catch (Exception e) {
+                    	System.out.println(str + " is not a number.");
+                    }
+                    break;
             }
         }
-        
-        return pino.pop();	//Palautetaan pinon päällimmäinen luku esimerkiksi syötteellä "8 10 2 3 5 4 + *" pinon lopputulos on "8 10 2 27".
-        					//Jämä luvut jää lillumaan, koska ne ovat ns virhesyöttöä.
+        //Palautetaan pinon päällimmäinen luku esimerkiksi syötteellä
+        //"8 10 2 3 5 4 + *" pinon lopputulos on "8 10 2 27".
+        //Jämäluvut jää lillumaan, koska ne ovat ns virhesyöttöä.
+        return pino.pop();
     }
 }
